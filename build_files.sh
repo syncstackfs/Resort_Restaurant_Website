15a4000829ee73bc48d58ae849a743b6ff2a31da
syncstackfs <syncstackfs@gmail.com>
2026-06-18 01:18:46 +0530

    fix vercel deployment structure

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-
-python manage.py collectstatic --noinput
-python manage.py migrate
+pip install -r requirements.txt
+python msresort/manage.py collectstatic --noinput --settings=msresort.settings
+python msresort/manage.py migrate --noinput --settings=msresort.settings
